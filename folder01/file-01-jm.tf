b0877a391dfe48aa9d2f0edec6dddef2839797cd
jumu103 <jumu103@users.noreply.github.com>
2021-07-12 17:16:58 +0200

    Update file-01-jm.tf

--- folder01/file-01-jm.tf
+++ folder01/file-01-jm.tf
@@ -19,33 +19,4 @@ data "vsphere_datastore" "datastore" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
-data "vsphere_resource_pool" "pool" {
-  name          = "Cluster01"
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
 
-data "vsphere_network" "network" {
-  name          = "SEG.172.16.10.00/24""
-  datacenter_id = "${data.vsphere_datacenter.dc.id}"
-}
-
-resource "vsphere_virtual_machine" "vm" {
-  name             = var.vm_name
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id     = "${data.vsphere_datastore.datastore.id}"
-
-  num_cpus = 1
-  memory   = 1024
-  guest_id = "other3xLinux64Guest"
-
-  network_interface {
-    network_id = "${data.vsphere_network.network.id}"
-  }
-
-  disk {
-    label = "disk0"
-    size  = 10
-  }
-
-  wait_for_guest_net_timeout    = -1
-}
