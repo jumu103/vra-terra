c7af8854523d55747db8533896a8f731e5a5e215
jumu103 <jumu103@users.noreply.github.com>
2021-07-13 07:28:02 +0200

    Update file-01-jm.tf

--- folder01/file-01-jm.tf
+++ folder01/file-01-jm.tf
@@ -4,6 +4,53 @@ variable "vm_name" {
 }
 
 
+provider "vsphere" {
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "pDatacenter1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "Cluster-1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "SEG.172.16.09.00"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm_name
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 1
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 10
+  }
+
+  wait_for_guest_net_timeout    = -1
+}
+
+
 
 
 
